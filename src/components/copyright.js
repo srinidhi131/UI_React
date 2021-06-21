765afe43074dea80a5565fb6027a1118ddc0e0fc
srinidhi131 <66429963+srinidhi131@users.noreply.github.com>
2021-06-21 22:58:27 +0530

    Update copyright.js

--- src/components/copyright.js
+++ src/components/copyright.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Copyright() {
     return (
         <div>
-            <div className="cr" style = {{background: "red",color:"white",textAlign:"center",fontSize:"15px",padding:"2%"}}>Copyright <i className="fa fa-copyright" /> 2015 - Dwidasa Samsara Indonesia</div>
+            <div id = "copyright" style = {{height: "10vh",color:"black",textAlign:"center",fontSize:"15px",padding:"2%"}}>>Copyright <i className="fa fa-copyright" /> 2015 - Dwidasa Samsara Indonesia</div>
         </div>
     )
 }
